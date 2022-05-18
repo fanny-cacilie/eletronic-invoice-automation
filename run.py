a0cbe34b179277cfb0c2ce71367260a1ef47c624
Fanny Cacilie <caciliefanny@gmail.com>
2022-05-18 20:37:10 -0300

    Remove unused import

--- run.py
+++ run.py
@@ -5,7 +5,6 @@ import time
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.select import Select
 from webdriver_manager.chrome import ChromeDriverManager 
 
